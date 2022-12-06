bc905fed3c4863fe6e807c531767b8cf8d5fc23d
Vincent Neparidze <negator92@gmail.com>
2022-12-06 19:59:07 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/negator92/antivirus-go-downloader
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	fileName    string
+	fullURLFile string
+)
+
+func main() {
+
+	currentTime := time.Now()
+	fullURLFiles := []string{
+		"https://rescuedisk.s.kaspersky-labs.com/latest/krd.iso",
+		"https://download.geo.drweb.com/pub/drweb/livedisk/drweb-livedisk-900-cd.iso",
+	}
+
+	for _, fullURLFile = range fullURLFiles {
+
+		// Build fileName from fullPath
+		fileURL, err := url.Parse(fullURLFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		path := fileURL.Path
+		segments := strings.Split(path, "/")
+		fileName = segments[len(segments)-1]
+
+		//drweb contains "-", cut it
+		if strings.Contains(fileName, "-") {
+			fileNamePreParts := strings.Split(fileName, "-")
+			fileName = fileNamePreParts[0] + ".iso"
+		}
+
+		//paste date into fileName
+		fmt.Println(fileName)
+		fileNameParts := strings.Split(fileName, ".")
+		fmt.Println(fileNameParts)
+		fileNameParts = append(fileNameParts, "-"+currentTime.Format("2006-01-02")+".")
+		fmt.Println(fileNameParts)
+		// fmt.Scanln()
+		fileNameParts[len(fileNameParts)-2], fileNameParts[len(fileNameParts)-1] = fileNameParts[len(fileNameParts)-1], fileNameParts[len(fileNameParts)-2]
+		fmt.Println(fileNameParts)
+		// fmt.Scanln()
+		fileName = fileNameParts[0] + fileNameParts[len(fileNameParts)-2] + fileNameParts[len(fileNameParts)-1]
+		fmt.Println(fileName)
+		// fmt.Scanln()
+		// Create blank file
+		file, err := os.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		client := http.Client{
+			CheckRedirect: func(r *http.Request, via []*http.Request) error {
+				r.URL.Opaque = r.URL.Path
+				return nil
+			},
+		}
+		// Put content on file
+		resp, err := client.Get(fullURLFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		size, err := io.Copy(file, resp.Body)
+
+		defer file.Close()
+
+		fmt.Printf("Downloaded a file %s with size %d\n", fileName, size)
+	}
+
+}
